2e82aec3b7abd2a44f516566861e80e329cc4882
AliSangiyan <103169881+AliSangiyan@users.noreply.github.com>
2024-04-21 22:17:56 +0330

    Add files via upload

---
+++ سه تمرین/تمرین تشخیص سن/gat.py
@@ -0,0 +1,13 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ag1=44
+>>> ag2=14
+>>> ag3=5
+>>> if ag1>ag2:
+...     print("بزرگتر")
+... elif age1<ag2:
+...         print("نوجوان")
+... else :
+...         print("کودک")
+... 
+بزرگتر
---
+++ سه تمرین/تمرین قبولی درس/pass.py
@@ -0,0 +1,12 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def moadel(num1):
+...     if num1 <10:
+...         print("مشروط")
+...     else :
+...         print("پاس")
+... 
+...         
+>>> num2=14
+>>> moadel(num2)
+پاس
